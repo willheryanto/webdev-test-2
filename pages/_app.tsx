3bfcf67f4668dbc7322ea56bcce4443eb4151225
William Heryanto <will.heryanto@gmail.com>
2022-09-17 19:50:54 +0700

    chore: remove unused files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import { GlobalStyle } from 'components/GlobalStyles';
 import Navbar from 'components/Navbar';
 
